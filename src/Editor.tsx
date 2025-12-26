3b95f6edea9376cc84a1665ae9b064565170f204
Alexandru Scheusan <andu.scheusan@gmail.com>
2025-12-26 16:01:03 +0200

    added codemirror module

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -1,16 +1,26 @@
-// web/src/Editor.tsx
-import { onMount } from 'solid-js';
+import { onMount, createSignal } from 'solid-js';
+import { EditorState } from '@codemirror/state';
+import { EditorView } from '@codemirror/view';
+import { basicSetup } from '@codemirror/basic-setup';
+import { markdown } from '@codemirror/lang-markdown';
+import { oneDark } from '@codemirror/theme-one-dark';
 
 const Editor = () => {
   let ref: HTMLDivElement | null = null;
+  const [view, setView] = createSignal<EditorView>();
 
   onMount(() => {
+    const startState = EditorState.create({
+      doc: '# Hello Aqua\nStart typing in markdown…',
+      extensions: [basicSetup, markdown(), oneDark],
+    });
     if (ref) {
-      ref.innerText = 'Replace me with CodeMirror in 1.2';
+      const v = new EditorView({ state: startState, parent: ref });
+      setView(v);
     }
   });
 
-  return <div ref={(el) => (ref = el)} class="editor" style={{ height: '100%' }} />;
+  return <div ref={(el) => (ref = el)} class="editor" />;
 };
 
 export default Editor;
