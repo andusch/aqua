78b823ada947ed68f8005625e515dfafff833193
Ioan-Alexandru Scheusan <andu.scheusan@gmail.com>
2026-01-20 11:14:00 +0200

    refreshed file tree after new file in folder

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
-import { createSignal, onMount } from 'solid-js';
+import { createSignal, onCleanup, onMount } from 'solid-js';
 import { invoke } from '@tauri-apps/api/core';
+import { listen } from '@tauri-apps/api/event';
 
 interface SidebarProps {
   onFileSelect: (path: string) => void;
@@ -7,15 +8,41 @@ interface SidebarProps {
 
 const Sidebar = (props: SidebarProps) => {
   const [files, setFiles] = createSignal<string[]>([]);
+  const [currentFolder, setCurrentFolder] = createSignal<string | null>(null);
+
+  const refreshFiles = async () => {
+    const folder = currentFolder();
+    if(!folder) return;
+    try {
+      const fileList = await invoke<string[]>('list_files', { path: folder });
+      setFiles(fileList);
+    } catch (err){
+      console.error("Failed to refresh files:", err);
+    }
+  };
 
   const pickFolder = async () => {
     try {
       const fileList = await invoke<string[]>('open_folder_and_list_files');
-      setFiles(fileList);
+      if (fileList && fileList.length > 0) {
+        const firstFilePath = fileList[0];
+        const folderPath = firstFilePath.substring(0, firstFilePath.lastIndexOf('/'));
+
+        setCurrentFolder(folderPath);
+        setFiles(fileList);
+      }
     } catch (err) {
-      console.log("Folder pick cancelled or failed:", err);
+      if (err !== "cancelled") console.log("Error:", err);
     }
+    
   }
+
+  onMount(async () => {
+    const unListen = await listen('refresh-files', () => {
+      refreshFiles();
+    });
+    onCleanup(() => unListen());
+  })
 
   return (
     <div class="sidebar">
